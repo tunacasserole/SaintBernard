840d61a8b05236bc4d6c5faa6bfa25b27078a123
AA-Ron <aaron.henderson@rfa.sc.gov>
2019-10-26 14:02:42 -0600

    seed data for demo transfer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Build Setup Data
+Allergy.create(description: 'hypersensitivity to aspirin')
+Allergy.create(description: 'NSAIDs and gluten intolerance')
+Diagnosis.create(code: 'J45', description: 'Asthma', coding_system: 'CHRONIC')
+ad = Diagnosis.create(code: 'S82.101', description: 'a fracture of upper end of the right tibia')
+pd = Diagnosis.create(code: 'S82.101A', description: 'a closed fracture in the right tibia')
+DiagnosticProcedure.create(description: 'exploratory radiography', moment: Time.now)
+Facility.create(name: 'Blue Alps Ski Camp')
+MedicationOrder.create(name: 'Acetaminophen', unit: 'mg', dosage: 500.00, route: 0, order_frequency: OrderFrequency.create(value: 'q4h'), necessity: 'to relieve swelling' )
+Observation.create(description: 'No soft tissues were damaged')
+Symptom.create(description: 'severe pain')
+Symptom.create(description: 'swelling and limited bending of the joint')
+Treatment.create(description: 'temporary bracing the right leg', necessity: 'restrict the motion')
+Admission.create(moment: Time.now)
+
+# Build Patient Records
+p=Patient.create(first_name: 'Thomas', last_name: 'Schudel', mr: '30997', dob: (Date.today - 43.years), gender: 1, admission: Admission.first )
+
+Allergy.all.each { |a| p.patient_allergies.create(allergy: a) }
+p.patient_medication_orders.create(medication_order: MedicationOrder.first)
+p.patient_diagnoses.create(diagnosis: Diagnosis.chronic.first )
+p.patient_diagnoses.create(diagnosis: pd )
+
+# Build Patient Admission Records
+pa = p.admission
+pa.admission_diagnoses.create(diagnosis: ad)
+Symptom.all.each { |s| pa.admission_symptoms.create(symptom: s) }
+pa.admission_observations.create(observation: Observation.first)
